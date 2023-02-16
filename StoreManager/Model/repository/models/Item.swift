47476ca211023913431c5065c7ae3c8b5ee2cbe4
Gaurav Mishra <gaurav.mishra@zestmoney.in>
2023-02-16 08:37:36 +0530

    created models

---
+++ StoreManager/Model/repository/models/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  StoreManager
+//
+//  Created by gaurav.mishra on 16/02/23.
+//
+
+import Foundation
+
+//Model to store an Item's detalis
+struct Item : Decodable
+{
+    //Properties to define and Item with name, price and extra data
+    let name: String
+    let price: String
+    let extra: String
+}
---
+++ StoreManager/Model/repository/models/ItemList.swift
@@ -0,0 +1,13 @@
+//
+//  ItemList.swift
+//  StoreManager
+//
+//  Created by gaurav.mishra on 16/02/23.
+//
+
+import Foundation
+
+struct ItemList
+{
+    let items: [Item]
+}
